2b1b85b742f4fa52c182514a90d296680718ea54
cj <2322507003@qq.com>
2024-12-30 15:11:50 +0800

    add sql

---
+++ sql/test.sql
@@ -0,0 +1,4 @@
+INSERT INTO `test` (`date`) VALUES ('2024-01-21 00:11:20');
+INSERT INTO `test` (`date`) VALUES ('2024-01-21 01:11:20');
+INSERT INTO `test` (`date`) VALUES ('2024-01-21 13:11:20');
+INSERT INTO `test` (`date`) VALUES ('2024-01-21 01:45:20');
